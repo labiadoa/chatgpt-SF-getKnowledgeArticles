351963e103af0d42368ce8365992132b53c88e88
labiadoa <129003942+labiadoa@users.noreply.github.com>
2023-03-30 14:14:20 +0200

    Update salesforce_etl.py

--- salesforce_etl.py
+++ salesforce_etl.py
@@ -1,113 +1,75 @@
 import os
-import json
-import time
-from datetime import datetime
 import requests
+from flask import Flask, request, jsonify
 from simple_salesforce import Salesforce
+import json
 
-# Variables de entorno
-SALESFORCE_USERNAME = os.environ["SALESFORCE_USERNAME"]
-SALESFORCE_PASSWORD = os.environ["SALESFORCE_PASSWORD"]
-SALESFORCE_SECURITY_TOKEN = os.environ["SALESFORCE_SECURITY_TOKEN"]
-SALESFORCE_ORG_TYPE = os.environ["SALESFORCE_ORG_TYPE"] # Posibles valores aceptados: "test" o "production"
-API_UPSERT_URL = os.environ["API_UPSERT_URL"]
-BEARER_TOKEN_UPSERT = os.environ["BEARER_TOKEN_UPSERT"]
-PERIODICITY_MINUTES = int(os.environ["PERIODICITY_MINUTES"])
-INSERTED_IDS_FILE = "inserted_ids.json"
+app = Flask(__name__)
 
-# Determinar la URL de autenticación según SALESFORCE_ORG_TYPE
-if SALESFORCE_ORG_TYPE.lower() == "test":
-    SALESFORCE_LOGIN_URL = "https://test.salesforce.com"
-elif SALESFORCE_ORG_TYPE.lower() == "production":
-    SALESFORCE_LOGIN_URL = "https://login.salesforce.com"
+SALESFORCE_USERNAME = os.environ.get("SALESFORCE_USERNAME")
+SALESFORCE_PASSWORD = os.environ.get("SALESFORCE_PASSWORD")
+SALESFORCE_SECURITY_TOKEN = os.environ.get("SALESFORCE_SECURITY_TOKEN")
+SALESFORCE_ORG_TYPE = os.environ.get("SALESFORCE_ORG_TYPE")
+
+if SALESFORCE_ORG_TYPE == "Test":
+    SALESFORCE_DOMAIN = "test"
 else:
-    raise ValueError("SALESFORCE_ORG_TYPE debe ser 'test' o 'production'")
+    SALESFORCE_DOMAIN = "login"
 
-# Conectar a Salesforce
 sf = Salesforce(
     username=SALESFORCE_USERNAME,
     password=SALESFORCE_PASSWORD,
     security_token=SALESFORCE_SECURITY_TOKEN,
-    sandbox=SALESFORCE_ORG_TYPE.lower() == "test"
+    domain=SALESFORCE_DOMAIN,
 )
 
-def load_inserted_ids():
-    if os.path.exists(INSERTED_IDS_FILE):
-        with open(INSERTED_IDS_FILE, "r") as file:
-            return json.load(file)
-    return {}
+@app.route('/salesforce-etl', methods=['POST'])
+def salesforce_etl():
+    url = os.environ.get("EXTERNAL_API_URL")
+    bearer_token = os.environ.get("EXTERNAL_API_BEARER_TOKEN")
 
-def save_inserted_ids(inserted_ids):
-    with open(INSERTED_IDS_FILE, "w") as file:
-        json.dump(inserted_ids, file)
+    query = "SELECT ArchivedDate,ArticleCreatedDate,ArticleMasterLanguage,ArticleNumber,ArticleTotalViewCount,CreatedDate,FirstPublishedDate,Id,IsDeleted,IsLatestVersion,VersionNumber,ValidationStatus,IsVisibleInApp,IsVisibleInCsp,IsVisibleInPkb,IsVisibleInPrm,KnowledgeArticleId,Language,LastModifiedById,LastModifiedDate,LastPublishedDate,PublishStatus,RecordTypeId,Answer__c,Question__c,retrievalAPISynced__c,wantSyncRetrievalAPI__c FROM Knowledge__kav WHERE PublishStatus = 'Online'"
+    sf_records = sf.query_all(query)['records']
 
-def fetch_salesforce_data():
-    query = """SELECT ArchivedDate,ArticleCreatedDate,ArticleMasterLanguage,ArticleNumber,ArticleTotalViewCount,CreatedDate,FirstPublishedDate,Id,IsDeleted,IsLatestVersion,VersionNumber,ValidationStatus,IsVisibleInApp,IsVisibleInCsp,IsVisibleInPkb,IsVisibleInPrm,KnowledgeArticleId,Language,LastModifiedById,LastModifiedDate,LastPublishedDate,PublishStatus,RecordTypeId,Answer__c,Question__c,retrievalAPISynced__c,wantSyncRetrievalAPI__c FROM Knowledge__kav WHERE PublishStatus = 'Online'"""
-    result = sf.query_all(query)
-    return result["records"]
-
-def create_documents(articles, inserted_ids):
     documents = []
-    for article in articles:
-        if article["Id"] not in inserted_ids:
+    for record in sf_records:
+        if not record['retrievalAPISynced__c']:
             document = {
-                "id": article["KnowledgeArticleId"],
-                "text": f"Pregunta: {article['Question__c']}. Respuesta: {article['Answer__c']}",
+                "id": record['KnowledgeArticleId'],
+                "text": f"Pregunta: {record['Question__c']} Respuesta: {record['Answer__c']}",
                 "metadata": {
-                    "ArticleNumber": article["ArticleNumber"],
-                    "Language": article["Language"],
-                    "RecordTypeId": article["RecordTypeId"],
-                    "Title": article["Title"],
-                    "Id": article["Id"],
-                    "ArticleCreatedDate": article["ArticleCreatedDate"],
-                    "CreatedDate": article["CreatedDate"],
-                    "FirstPublishedDate": article["FirstPublishedDate"],
-                    "LastModifiedDate": article["LastModifiedDate"],
-                    "LastPublishedDate": article["LastPublishedDate"],
-                    "ValidationStatus": article["ValidationStatus"],
-                    "VersionNumber": article["VersionNumber"]
+                    "ArticleNumber": record['ArticleNumber'],
+                    "Language": record['Language'],
+                    "RecordTypeId": record['RecordTypeId'],
+                    "Title": record['Title'],
+                    "Id": record['Id'],
+                    "ArticleCreatedDate": record['ArticleCreatedDate'],
+                    "CreatedDate": record['CreatedDate'],
+                    "FirstPublishedDate": record['FirstPublishedDate'],
+                    "LastModifiedDate": record['LastModifiedDate'],
+                    "LastPublishedDate": record['LastPublishedDate'],
+                    "ValidationStatus": record['ValidationStatus'],
+                    "VersionNumber": record['VersionNumber']
                 }
             }
             documents.append(document)
-    return documents
 
-def upsert_documents(documents):
-    headers = {
-        "Authorization": f"Bearer {BEARER_TOKEN_UPSERT}",
-        "Content-Type": "application/json"
-    }
-    response = requests.post(
-        API_UPSERT_URL,
-        headers=headers,
-        json={"documents": documents}
-    )
+    if not documents:
+        return jsonify({"message": "No new records to process."}), 200
 
-    if response.status_code != 200:
-        response.raise_for_status()
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    data = {"documents": documents}
+    response = requests.post(url + "/upsert", headers=headers, json=data)
 
-    return response
+    if response.status_code == 200:
+        synced_ids = response.json().get("ids", [])
+        for article_id in synced_ids:
+            sf.Knowledge__kav.update(article_id, {'retrievalAPISynced__c': True})
 
-def log_results(articles, upsert_response):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    num_articles_extracted = len(articles)
-    num_articles_inserted = len(upsert_response.json()["ids"])
+        return jsonify({"message": f"Processed {len(documents)} records, upserted {len(synced_ids)} records in the external API."}), 200
+    else:
+        return jsonify({"message": f"Error upserting records in the external API. Status code: {response.status_code}, Response: {response.text}"}), 500
 
-    with open("etl_results.log", "a") as log_file:
-        log_file.write(f"{timestamp} - Articles extracted: {num_articles_extracted}, Articles inserted: {num_articles_inserted}\n")
 
-def run_etl():
-    inserted_ids = load_inserted_ids()
-    articles = fetch_salesforce_data()
-    documents = create_documents(articles, inserted_ids)
-    upsert_response = upsert_documents(documents)
-    log_results(articles, upsert_response)
-
-    new_inserted_ids = upsert_response.json()["ids"]
-    for article_id, knowledge_article_id in zip([doc["metadata"]["Id"] for doc in documents], new_inserted_ids):
-        inserted_ids[article_id] = knowledge_article_id
-    save_inserted_ids(inserted_ids)
-
-if __name__ == "__main__":
-    while True:
-        run_etl()
-        time.sleep(PERIODICITY_MINUTES * 60)
+if __name__ == '__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
